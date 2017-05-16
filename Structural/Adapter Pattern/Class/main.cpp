25b3e4fbd9b7a3492515294180eaa5073066b719
Ahmet <asivri@fredonia.edu>
2017-05-16 22:08:36 +0300

    Class implementation of Adapter

---
+++ Structural/Adapter Pattern/Class/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ Adapter Pattern
+ Implement by Ahmet Sivri
+ Adapter Implementation Type: Class
+ Scenario: Suppose that there is a lecture and there is two types of students from Turkey and USA,
+ The lecture goes with English and Turkish ones cannot understand that.We need to have a translator
+ that will translate the lecture to the Turkish students.
+ In this example, a Student class will be the Target, the translator will be the Adapter,
+ and the Turkish students will be the Adaptee, like the class diagram below:
+ Story adapted from: qa.geeksforgeeks.org/562/what-is-adapter-design-pattern-a-practical-example
+ */
+
+
+//Target class
+class Student{
+public:
+    virtual string speaksEnglish() = 0;
+};
+
+//Adaptee Class
+class TurkishStudent{
+public:
+    string speaksTurkish()
+    {
+        cout<<"We are speaking Turkish"<<endl;
+        return "Merhaba!"; //It's kinda useless here.
+    }
+};
+
+//Adaptor class
+class Translator: public Student, private TurkishStudent{
+public:
+    string speaksEnglish()
+    {
+        //This part is the different than the object implementation of the pattern.
+        string _hello = speaksTurkish();
+        cout<<"Adaptor class has been called"<<endl;
+        cout<<"Now Turkish students are also able to understand the lecture!"<<endl;
+        return _hello;
+    }
+
+
+};
+
+//Main works as client here.
+int main()
+{
+    //Adapter creation
+    Student *sObj = new Translator();
+
+    string word = sObj->speaksEnglish();
+}
